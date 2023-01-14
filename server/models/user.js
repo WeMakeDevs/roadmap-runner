7551a7362ffd661cb465df9be682f4c10e7e8499
Kedar Basutkar <kedar-int@sharefable.com>
2023-01-14 11:03:11 +0530

    add: user schema

--- server/models/user.js
+++ server/models/user.js
@@ -37,42 +37,4 @@ const userSchema = new mongoose.Schema({
   progressStat: Number
 });
 
-// //encrypt password before save - HOOKS
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) {
-//     return next();
-//   }
-//   this.password = await bcrypt.hash(this.password, 10);
-// });
-
-// // validate the password with passed on user password
-// userSchema.methods.isValidatedPassword = async function (usersendPassword) {
-//   return await bcrypt.compare(usersendPassword, this.password);
-// };
-
-// //create and return jwt token
-// userSchema.methods.getJwtToken = function () {
-//   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRY,
-//   });
-// };
-
-// //generate forgot password token (string)
-// userSchema.methods.getForgotPasswordToken = function () {
-//   // generate a long and randomg string
-//   const forgotToken = crypto.randomBytes(20).toString("hex");
-
-//   // getting a hash - make sure to get a hash on backend
-//   this.forgotPasswordToken = crypto
-//     .createHash("sha256")
-//     .update(forgotToken)
-//     .digest("hex");
-
-//   //time of token
-//   this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000;
-
-//   return forgotToken;
-// };
-
 export default mongoose.model("User", userSchema);
-// module.exports = mongoose.model("User", userSchema);
