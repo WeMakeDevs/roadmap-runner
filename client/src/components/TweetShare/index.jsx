c5ff8c22a712378a26bf8689f0883c36f35c0e3e
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-14 18:14:46 +0530

    fix: tweet share component

--- client/src/components/TweetShare/index.jsx
+++ client/src/components/TweetShare/index.jsx
@@ -4,8 +4,17 @@ import "./index.css";
 const ShareTweet = ({
   shareText = "Hello there 👋 I'm following and learning through the roadmaps. Join my journey!",
 }) => {
+  function shareProgressUrl() {
+    return "http://twitter.com/share?text=" + encodeURIComponent(shareText);
+  }
+
   return (
-    <a className="share-tweet" href={shareText} target="_blank" rel="noreferrer">
+    <a
+      className="share-tweet"
+      href={shareProgressUrl()}
+      target="_blank"
+      rel="noreferrer"
+    >
       Share your progress
       <i className="fa-brands fa-twitter"></i>
     </a>
