73bd579b6c56d019595767deda3ebb759798aade
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-14 06:33:22 +0530

    feat: navbar

---
+++ client/src/components/Sidebar/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Navbar />
     <App />
   </BrowserRouter>
 );
