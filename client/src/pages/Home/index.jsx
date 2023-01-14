ca3897b17f0ee6fce1797eff81fa2e457307144b
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-14 16:36:43 +0530

    style: home page

---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import SidebarLayout from "../../components/SidebarLayout";
+import ConsistencyGraph from "./ConsistencyGraph";
+import SmileyFace from "../../assets/mood/smileyface.svg";
+import "./index.css";
+import { LeaderboardIcon, MyRoadmapIcon, RoadmapIcon } from "../../assets/sidebar";
+
+const Home = () => {
+  return (
+    <SidebarLayout>
+      <div className="home-container">
+        <div>
+          <h1>Hello Siddhi 👋</h1>
+          <div className="stats-card">
+            <section className="roadmaps-card card">
+              <img src={RoadmapIcon} alt="" />
+              <h2 className="title">Your Roadmaps</h2>
+              <p className="count">2</p>
+            </section>
+            <section className="progress-card card">
+            <img src={LeaderboardIcon} alt="" />
+              <h2 className="title">Your Progress</h2>
+              <p className="count">10%</p>
+            </section>
+            <section className="progress-card card">
+              <img src={MyRoadmapIcon} alt="" />
+              <h2 className="title">Total Roadmaps</h2>
+              <p className="count">6</p>
+            </section>
+          </div>
+          <section className="contribution-graph">
+            <h2 className="title">Your consistency graph</h2>
+            <ConsistencyGraph />
+          </section>
+        </div>
+        <div className="daily-visit-card">
+          <div>
+            <img src={SmileyFace} alt="" />
+            <p>Keep Learning! Keep Growing!</p>
+          </div>
+        </div>
+      </div>
+    </SidebarLayout>
+  );
+};
+
+export default Home;
