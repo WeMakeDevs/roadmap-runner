e178cadce1e98e4d603abbca31fbf60eefcd67f8
Kedar Basutkar <kedar-int@sharefable.com>
2023-01-14 10:41:04 +0530

    add: mongo config

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import * as env from 'dotenv'
 env.config()
-import {app} from './app.js'
+import { connectWithDb } from './config/db.js'
+import { app } from './app.js'
 
-const PORT  = process.env.PORT || 5000
+connectWithDb()
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
 
