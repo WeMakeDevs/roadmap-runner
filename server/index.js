5f4341baad1a7f54356790dc2fa4c69dbf759ff8
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-14 10:08:11 +0530

    fix: default PORT

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { app } = require('./app')
 
-const { PORT } = process.env
+const PORT  = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
 
