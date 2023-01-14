a4c97831e9c5484609750306ab78119973a9d54f
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-14 11:46:49 +0530

    style: roadmap page

--- client/src/pages/Roadmap/index.jsx
+++ client/src/pages/Roadmap/index.jsx
@@ -21,13 +21,27 @@ const Roadmap = () => {
 
   return (
     <SidebarLayout>
-      <h1>
-        {roadmap?.name} {id}
-      </h1>
-      <div className="roadmap">
-        {roadmap &&
-          roadmap.sections &&
-          roadmap.sections.map((section) => <Section section={section} />)}
+      <div className="roadmap-container">
+        <div>
+          <h1>
+            {roadmap?.name} {id}
+          </h1>
+          <button className="roadmap-enroll-btn">Enroll</button>
+
+          <div className="roadmap">
+            {roadmap &&
+              roadmap.sections &&
+              roadmap.sections.map((section) => <Section section={section} />)}
+          </div>
+        </div>
+
+        <div className="roadmap-info">
+          <div>
+            <img src={roadmap?.bannerImage} alt="" />
+            <h2 style={{color: "white", marginBottom: "1rem", fontSize: "1.5rem"}}>{roadmap?.name}</h2>
+            <p className="description">{roadmap?.description}</p>
+          </div>
+        </div>
       </div>
     </SidebarLayout>
   );
