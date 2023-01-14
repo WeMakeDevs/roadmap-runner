52ff879e28d61a34b5c3e32fba0c709690836575
Kedar Basutkar <kedar-int@sharefable.com>
2023-01-14 13:10:12 +0530

    use user router

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import user from './routes/user.js'
 
 const app = express()
 
@@ -8,6 +9,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('tiny'))
+
+//router middleware
+app.use("/api/v1", user);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'hello world!' });
