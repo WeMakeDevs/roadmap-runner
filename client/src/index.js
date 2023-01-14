11b406f14ee6418bce01c62d7918fd1d4778be1b
Kedar Basutkar <kedar-int@sharefable.com>
2023-01-14 11:48:29 +0530

    Apply AuthContextProvider to main App

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Navbar />
-    <App />
+    <AuthContextProvider>
+      <Navbar />
+      <App />
+    </AuthContextProvider>
   </BrowserRouter>
 );
