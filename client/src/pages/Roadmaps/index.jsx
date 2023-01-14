71285791b46e307710c805da712aeee19959f484
Kedar Basutkar <kedar-int@sharefable.com>
2023-01-14 15:55:35 +0530

    integrate roadmaps api in client

--- client/src/pages/Roadmaps/index.jsx
+++ client/src/pages/Roadmaps/index.jsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
 import SidebarLayout from "../../components/SidebarLayout";
-import roadmapsData from "../../data/roadmaps.json";
 import Card from "./Card";
+import api from "../../api";
+import { useAuthContext } from "../../hooks/useAuthContext";
 import "./index.css";
 
 const Roadmaps = () => {
   const [roadmaps, setRoadmaps] = useState();
+  const { user } = useAuthContext();
 
   useEffect(() => {
     async function fetchRoadmaps() {
-      setRoadmaps(roadmapsData.data.roadmaps);
+      const res = await api.get("roadmap", {
+        headers: { Authorization: user.accessToken },
+      });
+      setRoadmaps(res.data.roadmaps);
     }
 
     fetchRoadmaps();
-  }, []);
+  }, [user]);
 
   return (
     <SidebarLayout>
@@ -23,9 +28,10 @@ const Roadmaps = () => {
           {roadmaps &&
             roadmaps.map((roadmap) => (
               <Card
+                key={roadmap._id}
                 bannerImage={roadmap.bannerImage}
                 title={roadmap.name}
-                id={roadmap.id}
+                id={roadmap._id}
                 tagline={roadmap.tagline}
               />
             ))}
