39616a5cba15a57eb1084111789c6c8852bfb282
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-14 09:44:01 +0530

    style: roadmaps page

---
+++ client/src/pages/Roadmaps/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Card = ({ bannerImage, id, title, tagline }) => {
+  return (
+    <Link to={`/roadmaps/${id}`}>
+      <section className="roadmap-card">
+        <img src={bannerImage} alt="" />
+        <h2>{title}</h2>
+        <p>{tagline}</p>
+      </section>
+    </Link>
+  );
+};
+
+export default Card;
---
+++ client/src/pages/Roadmaps/index.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import SidebarLayout from "../../components/SidebarLayout";
+import roadmapsData from "../../data/roadmaps.json";
+import Card from "./Card";
+import "./index.css";
+
+const Roadmaps = () => {
+  const [roadmaps, setRoadmaps] = useState();
+
+  useEffect(() => {
+    async function fetchRoadmaps() {
+      setRoadmaps(roadmapsData.data.roadmaps);
+    }
+
+    fetchRoadmaps();
+  }, []);
+
+  return (
+    <SidebarLayout>
+      <h1>Roadmaps</h1>
+      <div className="roadmaps-container">
+        <div className="roadmaps-grid">
+          {roadmaps &&
+            roadmaps.map((roadmap) => (
+              <Card
+                bannerImage={roadmap.bannerImage}
+                title={roadmap.name}
+                id={roadmap.id}
+                tagline={roadmap.tagline}
+              />
+            ))}
+        </div>
+      </div>
+    </SidebarLayout>
+  );
+};
+
+export default Roadmaps;
