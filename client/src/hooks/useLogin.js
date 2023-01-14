802057a8d61991f69661c6bf639f87ff93e48626
Kedar Basutkar <kedar-int@sharefable.com>
2023-01-14 13:10:45 +0530

    update useLogin hook to make API call

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import { auth } from "../firebase/config";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useAuthContext } from "./useAuthContext";
+import api from "../api";
 
-const provider = new GoogleAuthProvider()
+const provider = new GoogleAuthProvider();
 
 export const useLogin = () => {
   const [isCancelled, setIsCancelled] = useState(false);
@@ -17,10 +18,16 @@ export const useLogin = () => {
 
     try {
       // login
-      const res = await signInWithPopup(auth, provider)
+      const res = await signInWithPopup(auth, provider);
+      const userBody = {
+        name: res.user.displayName,
+        email: res.user.email,
+        displayPicture: res.user.photoURL,
+      };
+      const apiRes = await api.post("signup", userBody);
 
       // dispatch login action
-      dispatch({ type: "LOGIN", payload: res.user });
+      dispatch({ type: "LOGIN", payload: { id: apiRes.data.user._id, ...res.user } });
 
       if (!isCancelled) {
         setIsPending(false);
