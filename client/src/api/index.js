53fff118f4e17931a274e1b56b5d069a6ea21395
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-15 11:02:12 +0530

    fix: api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://localhost:8001/api/v1/`
+  baseURL: `https://roadmap-runner-server.onrender.com/api/v1`,
 });
