d6d0ddbb907a0baae6c9ca1ca0055e0259ec9d64
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-15 06:40:46 +0530

    feat: my roadmap progress

--- client/src/components/Confetti/index.jsx
+++ client/src/components/Confetti/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Confetti from 'react-confetti';
+import ReactDOM from "react-dom";
 
 const index = () => {
-  return (
+  return ReactDOM.createPortal(
     <Confetti
             width={window.innerWidth - 100}
             height={window.innerHeight}
             recycle={false}
-    />
-  )
+    />, document.body)
 }
 
 export default index
--- client/src/pages/MyRoadmap/Resource.jsx
+++ client/src/pages/MyRoadmap/Resource.jsx
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from "react";
 import api from "../../api";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useParams } from "react-router";
+import Modal from "../../components/Modal";
+import Confetti from "../../components/Confetti";
+import { Link } from "react-router-dom";
 
 const Resource = ({ resource, completed = false }) => {
   const { user } = useAuthContext();
   const [checked, setChecked] = useState(completed);
+  const [showCompletedMsg, setShowCompletedMsg] = useState(false);
   const { id } = useParams();
 
   const toggleCompleted = async () => {
@@ -13,21 +17,52 @@ const Resource = ({ resource, completed = false }) => {
   };
 
   useEffect(() => {
-    const postProgress = () => {
-      api.post(
+    const postProgress = async () => {
+      await api.post(
         "progress",
         { resourceId: resource._id, roadmapId: id },
         {
           headers: { Authorization: user.accessToken },
         }
       );
+      setShowCompletedMsg(true);
     };
     if (checked) {
       postProgress();
     }
   }, [checked, user, id, resource]);
 
+  if (checked) {
+    return (
+      <>
+        <div className="resource card">
+          <div>
+            <label className="checkbox-container tooltip">
+              <input type="checkbox" className="form-control" checked={true} />
+              <span className="checkbox-custom"></span>
+            </label>
+            {resource.id} {resource.title}
+          </div>
+          <a href={resource.url} target="_blank" rel="noreferrer">
+            <i className="fa-solid fa-arrow-up-right-from-square"></i>
+          </a>
+        </div>
+        {showCompletedMsg && (
+          <>
+            <Confetti />
+            <Modal closeModal={() => setShowCompletedMsg(false)}>
+              <h2>Success!</h2>
+              <p>You have completed one more section! Share your progress 🤗</p>
+              <Link to={`/myroadmaps`}>Share</Link>
+            </Modal>
+          </>
+        )}
+      </>
+    );
+  }
+
   return (
+    <>
     <div
       className="resource card"
       style={{ cursor: "pointer" }}
@@ -50,6 +85,17 @@ const Resource = ({ resource, completed = false }) => {
         <i className="fa-solid fa-arrow-up-right-from-square"></i>
       </a>
     </div>
+    {showCompletedMsg && (
+        <>
+          <Confetti />
+          <Modal closeModal={() => setShowCompletedMsg(false)}>
+            <h2>Success!</h2>
+            <p>You have completed one more section! Share your progress 🤗</p>
+            <Link to={`/myroadmaps`}>Share</Link>
+          </Modal>
+        </>
+      )}
+    </>
   );
 };
 
