29d35e4b183ba5a1b9d1588a2469124b4e10784f
GiriMadhan <girimadhan7@gmail.com>
2023-05-16 15:54:39 +0530

    Added alt text to images.

--- client/src/pages/MyRoadmaps/Card.jsx
+++ client/src/pages/MyRoadmaps/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ bannerImage, id, title, tagline, progress = 0 }) => {
   return (
     <Link to={`/myroadmaps/${id}`}>
       <section className="roadmap-card">
-        <img src={bannerImage} alt="" />
+        <img src={bannerImage} alt="My Roadmap banner" />
         <h2>{title}</h2>
         <p className="tagline">{tagline}</p>
         <p className="completed">{progress} sections completed</p>
